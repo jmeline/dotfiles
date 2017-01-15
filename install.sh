4ff3d101db05b403d9c9849b5063bd3981f2a1b4
jmeline <jacob.meline@gmail.com>
2017-01-15 16:24:18 -0700

    Adding install scripts for fonts, neovim, and termite

---
+++ install.sh
@@ -0,0 +1,7 @@
+# install.sh
+
+source ./scripts/install_fonts.sh
+source ./scripts/install_termite.sh
+source ./scripts/install_neovim.sh
+source ./scripts/install_neovim.sh
+
---
+++ scripts/install_termite.sh
@@ -0,0 +1,4 @@
+# install termite
+sudo pacman -S termite
+mkdir -p ~/.config/termite
+ln -sf $(pwd)/termite/config ~/.config/termite/config
