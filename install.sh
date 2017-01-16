3aba2a8c6f57bdb327ec20d1e6cf2b4069aebc3a
jmeline <jacob.meline@gmail.com>
2017-01-16 14:20:40 -0700

    Adding updates to the install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 # install.sh
 
+source ./scripts/install_zprezto.sh
 source ./scripts/install_fonts.sh
 source ./scripts/install_termite.sh
 source ./scripts/install_neovim.sh
-source ./scripts/install_neovim.sh
+source ./scripts/install_tmux.sh
 
