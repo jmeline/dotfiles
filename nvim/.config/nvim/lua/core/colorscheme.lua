55fc7894d5d7c897f9702f0de70738ae1d862aa5
Jacob Meline <jacob.meline@gmail.com>
2022-04-21 22:50:03 -0600

    Remove nvim config. Moved to a different repo

--- nvim/.config/nvim/lua/core/colorscheme.lua
+++
@@ -1,6 +0,0 @@
-local opt = vim.opt  -- to set options
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-
--- load up personal colorscheme --
-opt.runtimepath:append("~/.config/nvim/dracula_pro/vim")
-cmd 'colorscheme dracula_pro_buffy'
