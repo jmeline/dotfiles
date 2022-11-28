f5a515baf3c964cb0fc610846dd80982e70079d9
Jacob Meline <jacob.meline@gmail.com>
2022-11-28 00:57:08 -0700

    add minor update to updatescreen.sh

--- bspwm/.config/bspwm/updateScreen.sh
+++ bspwm/.config/bspwm/updateScreen.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-RIGHT_MONITOR=DisplayPort-0
-LEFT_MONITOR=HDMI-A-0
+# RIGHT_MONITOR=DisplayPort-1
+LEFT_MONITOR=DisplayPort-0
+# LEFT_MONITOR=HDMI-A-0
 # TV=DVI-I-0
   # xrandr --output DVI-I-1 --mode 1920x1080 --set underscan off --primary --output HDMI-A-0 --mode 1920x1080 --right-of DVI-I-1 --output DVI-I-0 --mode 1920x1080 --right-of HDMI-A-0 --set underscan off --set "underscan vborder" 35 --set "underscan hborder" 50
-
-xrandr \
-  --output $LEFT_MONITOR  --mode 2560x1440 --primary \
-  --output $RIGHT_MONITOR --mode 2560x1440 --right-of $LEFT_MONITOR \
+#
+xrandr --output $LEFT_MONITOR  --mode 2560x1440 --primary
+# xrandr \
+#   --output $LEFT_MONITOR  --mode 2560x1440 --primary \
+#   --output $RIGHT_MONITOR --mode 2560x1440 --right-of $LEFT_MONITOR \
   # --output DVI-I-1        --mode 1920x1080 --right-of $RIGHT_MONITOR \
   #   --set underscan on            \
     # --set "underscan vborder" 35  \
     # --set "underscan hborder" 40
 
 
-nitrogen --restore
+# nitrogen --restore
