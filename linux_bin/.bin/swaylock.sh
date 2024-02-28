61593275a5638c3e6bb625eec090dc1585c2f3e5
Jacob Meline <jacob.meline@gmail.com>
2024-02-28 00:11:14 -0700

    Add swaylock config

---
+++ linux_bin/.bin/swaylock.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+swaylock \
+    --clock \
+    --indicator-thickness 15 \
+    --indicator-radius 100 \
+    --ring-color bb00cc \
+    --key-hl-color 880033 \
+    --image ~/pictures/wallhaven/wallhaven-428zl9_2560x1440.png
+    # --daemonize
